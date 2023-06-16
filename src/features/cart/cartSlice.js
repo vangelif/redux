3480f2c86535f2b46c413e481cfd4ed94a97f7c2
vangelif <ev.fotiades@yahoo.com>
2023-06-16 22:52:39 +0300

    learn useSelector and icons

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cartItems: [],
+  amount: 12,
+  total: 0,
+  isLoading: true,
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+});
+
+// console.log(cartSlice);
+
+export default cartSlice.reducer;
+
--- src/icons.js
+++ src/icons.js
@@ -1,17 +1,17 @@
 export const CartIcon = () => {
   return (
     <svg
-      xmlns='http://www.w3.org/2000/svg'
-      className='h-6 w-6 '
-      fill='none'
-      viewBox='0 0 24 24'
-      stroke='currentColor'
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-6 w-6 "
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
       strokeWidth={2}
     >
       <path
-        strokeLinecap='round'
-        strokeLinejoin='round'
-        d='M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z'
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
       />
     </svg>
   );
@@ -20,14 +20,14 @@ export const CartIcon = () => {
 export const ChevronDown = () => {
   return (
     <svg
-      xmlns='http://www.w3.org/2000/svg'
-      className='h-6 w-6'
-      fill='none'
-      viewBox='0 0 24 24'
-      stroke='currentColor'
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
       strokeWidth={2}
     >
-      <path strokeLinecap='round' strokeLinejoin='round' d='M19 9l-7 7-7-7' />
+      <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
     </svg>
   );
 };
@@ -35,14 +35,32 @@ export const ChevronDown = () => {
 export const ChevronUp = () => {
   return (
     <svg
-      xmlns='http://www.w3.org/2000/svg'
-      className='h-6 w-6'
-      fill='none'
-      viewBox='0 0 24 24'
-      stroke='currentColor'
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
       strokeWidth={2}
     >
-      <path strokeLinecap='round' strokeLinejoin='round' d='M5 15l7-7 7 7' />
+      <path strokeLinecap="round" strokeLinejoin="round" d="M5 15l7-7 7 7" />
     </svg>
   );
 };
+
+export const Testing = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="w-6 h-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M6 13.5V3.75m0 9.75a1.5 1.5 0 010 3m0-3a1.5 1.5 0 000 3m0 3.75V16.5m12-3V3.75m0 9.75a1.5 1.5 0 010 3m0-3a1.5 1.5 0 000 3m0 3.75V16.5m-6-9V3.75m0 3.75a1.5 1.5 0 010 3m0-3a1.5 1.5 0 000 3m0 9.75V10.5"
+    />
+  </svg>
+);
+
